067a2524baf2309c686c924108240f9d5ea4aae5
Diptajyoti Mukherjee <diptajym@coma.hpc1.cs.cmu.edu>
2020-01-12 16:10:04 -0500

    added initial c interface

---
+++ PyFalcon/src/interface.c
@@ -0,0 +1,18 @@
+// TODO:
+// add functions to set values from the python interface to the C interface
+// add functions to get values from the C interface to the python interface
+//
+
+
+void __set_input_params__(int N, double eta, int out_n, double final_t) {
+
+}
+
+
+void __set_particle_params__(double *x, double* y, double* z,
+        double* vx, double* vy, double* vz,
+        double *m) {
+
+}
+
+
